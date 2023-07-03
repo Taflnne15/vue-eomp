8792facd0b4ec40455ad8b36c17fc1b7498dd908
Taflnne15 <taflynnepetersen@gmail.com>
2023-07-03 09:03:34 +0200

    Home and About start up

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,14 +3,30 @@
     <div class="container text-center">
   <div class="row align-items-center">
     <div class="col">
-    <p>azsqxdcwfgrbhjynmhgfdeerfcjehrhvtr</p>
+      <h1>Hello learn more about me:</h1>
+    <h4>My name is Taflynne Summer Petersen , I am 18 years of age and an aspiring Full Stack Developer. <br>
+          Along with coding, I am a musician as well,my two instruments are Bass Guitar and Guitar.</h4>
     </div>
   
     <div class="col">
-      <img src="https://i.postimg.cc/6QXN9vTS/taff-portfolio-img.jpg" alt="img" width="200px
+      <img src="https://i.postimg.cc/6QXN9vTS/taff-portfolio-img.jpg" alt="img" width="200px 
       ">
     </div>
   </div>
 </div>
   </div>
 </template>
+<style  scoped>
+img{
+  margin-top: 100px;
+  border-radius: 50%;
+  width: 300px;  
+}
+h1{
+font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+h4{
+font-family: Arial, Helvetica, sans-serif;
+
+}
+</style>
