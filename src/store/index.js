b6a138587e339dfc1b579d004e3e7e8c89491c07
Taflnne15 <taflynnepetersen@gmail.com>
2023-07-03 16:52:58 +0200

    almost done

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,49 @@
-import { createStore } from 'vuex'
 
+import { createStore } from 'vuex'
+const dataLink = "https://taflnne15.github.io/vue_eompData/data/"
 export default createStore({
   state: {
-  },
+    testimonials:null,
+    projects: null
+  }, 
+
   getters: {
   },
   mutations: {
+    setTestimonials(state, testimonials){
+      state.testimonials = testimonials;
+    }, setProjects (state, projects){
+      state.projects = projects
+    },
+  
+    
+
+
+  
   },
   actions: {
-  },
+    async fetchTestimonials(context){
+      try{
+        let res = await fetch(dataLink);
+        let { Testimonials } = await res.json()
+        context.commit('setTestimonials' ,Testimonials)
+      }
+      catch(e){
+        console.log(e.message);
+      }
+    } ,
+    async fetchProjects(context){
+      try{
+        let res = await fetch (dataLink);
+        let {Projects} = await res.json()
+        context.commit('setProjects', Projects)
+      }
+      catch(e){
+        console.log(e.message)
+      }
+    }
+  }, 
+
   modules: {
   }
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,8 @@
     <div class="col">
       <h1>Hello learn more about me:</h1>
     <h4>My name is Taflynne Summer Petersen , I am 18 years of age and an aspiring Full Stack Developer. <br>
-          Along with coding, I am a musician as well,my two instruments are Bass Guitar and Guitar.</h4>
+          Along with coding, I am a musician as well,my two instruments are bass guitar and guitar.I believe strongly I up skilling one-self.
+        I am very optimistic, but yet realistic and I enjoy a good challange where I know growth is certain.</h4>
     </div>
   
     <div class="col">
@@ -19,14 +20,14 @@
 <style  scoped>
 img{
   margin-top: 100px;
-  border-radius: 50%;
+  border-radius: 50% ;
   width: 300px;  
 }
 h1{
 font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
 h4{
-font-family: Arial, Helvetica, sans-serif;
+font-family:'Gill Sans', 'Gill Sans MT', 'Calibri' 'Trebuchet MS', sans-serif ;
 
 }
 </style>
